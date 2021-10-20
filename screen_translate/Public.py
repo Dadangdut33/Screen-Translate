0232253aed399bf20e1692833057518dc5c9d96b
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-10-20 21:20:27 +0700

    1.7.2 Release

--- screen_translate/Public.py
+++ screen_translate/Public.py
@@ -66,7 +66,7 @@ class global_Stuff:
         self.mboxOpen = False
         
         # Version
-        self.version = "1.7.1"
+        self.version = "1.7.2"
         self.versionType = "release"
         self.newVerStatusCache = None
 
