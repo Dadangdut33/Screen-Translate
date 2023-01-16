fb2490dd9fb8e1364579107918136388d53831cd
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2023-01-16 17:23:31 +0700

    capture #14

--- screen_translate/utils/Capture.py
+++ screen_translate/utils/Capture.py
@@ -1,25 +1,22 @@
-# Internal
 import os
 from datetime import datetime
 
-# External
 import numpy as np
 import pyautogui
 import pytesseract
 import cv2
 
-# Lang Code
-from screen_translate.LangCode import *
+from screen_translate.Logging import logger
+from screen_translate.components.MBox import Mbox
+from screen_translate.Globals import fJson, dir_captured
+from .Helper import startFile
+from .LangCode import tesseract_lang
 
-# Mbox
-from screen_translate.Mbox import Mbox
+# Settings to capture all screens
+from PIL import ImageGrab
+from functools import partial
 
-# Public methods
-from screen_translate.Public import startfile
-
-# Get Path
-dir_path = os.path.dirname(os.path.realpath(__file__))
-img_captured_path = os.path.join(dir_path, "../img_captured")
+ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
 
 
 def createPicDirIfGone():
@@ -27,35 +24,28 @@ def createPicDirIfGone():
     Create the directory if it does not exist
     """
     # Will create the dir if not exists
-    if not os.path.exists(img_captured_path):
+    if not os.path.exists(dir_captured):
         try:
-            os.makedirs(img_captured_path)
+            os.makedirs(dir_captured)
         except Exception as e:
-            print("Error: " + str(e))
+            logger.exception("Error: " + str(e))
             Mbox("Error: ", str(e), 2)
 
 
-def captureImg(coords, sourceLang, tesseract_Location, saveImg=False, enhance_WithCv2=False, grayScale=False, background=None, debugmode=False, replaceNewLine=True):
+def captureImg(coords, sourceLang):
     """Capture Image and return text from it
 
     Args:
         coords (int): Coordinates and size of the screen to capture (x,y,w,h)
         sourceLang (string): The Language to be translated
-        tesseract_Location (string): Tesseract .exe location
-        cached (bool, optional): Cache/Save Image or not. Defaults to False.
-        enhance_WithCv2 (bool, optional): Enhance Image with cv2. Defaults to False.
-        grayScale (bool, optional): Convert Image to GrayScale. Defaults to False.
-        background (string, optional): Background color. Defaults to None.
-        debugmode (bool, optional): Debug mode. Defaults to False.
-        replaceNewLine (bool, optional): Replace new line with space. Defaults to True.
     Returns:
         status, result: Success or Error, Result
     """
     # Language Code
     try:
-        langCode = tesseract_Lang[sourceLang]
+        langCode = tesseract_lang[sourceLang]
     except KeyError as e:
-        print("Error: Key Error\n" + str(e))
+        logger.exception("Error: Key Error\n" + str(e))
         Mbox("Key Error, On Assigning Language Code.\n" + str(e), "Error: Key Error", 2)
         return False, "Error: Key Error"
 
@@ -63,60 +53,62 @@ def captureImg(coords, sourceLang, tesseract_Location, saveImg=False, enhance_Wi
     wordsGet = ""
     try:
         # Capture the designated location
-        captured = pyautogui.screenshot(region=(coords[0], coords[1], coords[2], coords[3]))
+        captured = pyautogui.screenshot(region=(coords[0], coords[1], coords[2], coords[3]))  # type: ignore
 
-        # Set tesseract_Location
-        pytesseract.pytesseract.tesseract_cmd = tesseract_Location
+        # Set variables
+        pytesseract.pytesseract.tesseract_cmd = fJson.settingCache["tesseract_loc"]
+        enhance_withCv2 = fJson.settingCache["enhance_with_cv2_Contour"]
+        grayscale = fJson.settingCache["enhance_with_grayscale"]
+        debugmode = fJson.settingCache["enhance_debugmode"]
+        background = fJson.settingCache["enhance_background"]
+        replaceNewLine = fJson.settingCache["replaceNewLine"]
+        saveImg = fJson.settingCache["saveImg"]
+        saveName = os.path.join(dir_captured, "captured_" + datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".png")
 
         # Enhance with cv2 if selected
-        if enhance_WithCv2:
-            # Convert captured img to cv2 format
-            open_cv_image = np.array(captured)
-            # Convert RGB to BGR
-            open_cv_image = open_cv_image[:, :, ::-1].copy()
-            # Convert the image to gray scale
-            grayImg = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
-
-            # debug
-            if debugmode and grayScale:
-                cv2.imshow("Grayscale Image", grayImg)
+        if enhance_withCv2:
+            open_cv_image = np.array(captured)  # Convert captured img to cv2 format
+            open_cv_image = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
+            grayImg = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)  # Convert the image to gray scale
 
             # Threshtype
-            imgType = "Thresh Image"
             if background == "Auto-Detect":
+                logger.info("Detecting background color...")
                 is_light = np.mean(open_cv_image) > 127
-                imgType += " (Auto - Light)" if is_light else " (Auto - Dark)"
-                print(">> Image detected as light" if is_light else ">> Image detected as dark")
+                logger.debug(">> Image detected as light" if is_light else ">> Image detected as dark")
                 threshType = cv2.THRESH_BINARY_INV if is_light else cv2.THRESH_BINARY
             else:
-                threshType = cv2.THRESH_BINARY_INV if background == "Light" else cv2.THRESH_BINARY
+                is_light = background == "Light"
+                threshType = cv2.THRESH_BINARY_INV if is_light else cv2.THRESH_BINARY
 
             # Performing OTSU threshold
-            ret, thresh = cv2.threshold(grayImg, 0, 255, cv2.THRESH_OTSU | threshType)
-
-            # debug
-            if debugmode:
-                cv2.imshow(imgType, thresh)
+            logger.info("Performing OTSU threshold...")
+            ret, thresh = cv2.threshold(grayImg, 0, 255, cv2.THRESH_OTSU | threshType)  # TODO: CHECK what if use rgb img, because i forgor 💀
 
             # Specify structure shape and kernel size.
             # Kernel size increases or decreases the area
             # of the rectangle to be detected.
             # A smaller value like (10, 10) will detect
             # each word instead of a sentence.
+            logger.info("Creating structuring element...")
             rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
             # Applying dilation on the threshold image
+            logger.info("Applying dilation on the threshold image...")
             dilation = cv2.dilate(thresh, rectKernel, iterations=1)
 
             # debug
             if debugmode:
-                cv2.imshow("Dilation Image", dilation)
+                if grayscale:
+                    cv2.imshow("Grayscale Image", grayImg)
+                cv2.imshow("Thresh Image (Auto - Light)" if is_light else "Thresh Image (Auto - Dark)", thresh)
+                cv2.imshow("Dilated Image", dilation)
 
             # Finding contours in the image based on dilation
             contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
             # Create a copy of captured image
-            imgCopy = grayImg if grayScale else open_cv_image.copy()
+            imgFinal = grayImg if grayscale else open_cv_image.copy()
 
             # Looping through the identified contours
             # Then rectangular part is cropped and passed on
@@ -125,11 +117,11 @@ def captureImg(coords, sourceLang, tesseract_Location, saveImg=False, enhance_Wi
                 x, y, w, h = cv2.boundingRect(cnt)
                 # Drawing a rectangle on copied image
                 if debugmode:
-                    rect = cv2.rectangle(imgCopy, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    rect = cv2.rectangle(imgFinal, (x, y), (x + w, y + h), (0, 255, 0), 2)
                     cv2.imshow("Rectangle drawn on image", rect)
 
                 # Cropping the text block for giving input to OCR
-                cropped = imgCopy[y : y + h, x : x + w]
+                cropped = imgFinal[y : y + h, x : x + w]
 
                 # Apply OCR on the cropped image
                 text = pytesseract.image_to_string(cropped, langCode)
@@ -139,30 +131,28 @@ def captureImg(coords, sourceLang, tesseract_Location, saveImg=False, enhance_Wi
 
             if saveImg:
                 createPicDirIfGone()
-                captured.save(os.path.join(img_captured_path, "captured_" + datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".png"))
+                captured.save(saveName)
         else:
-            if grayScale:
-                # Convert captured img to cv2 format
-                open_cv_image = np.array(captured)
-                # Convert RGB to BGR
-                open_cv_image = open_cv_image[:, :, ::-1].copy()
+            if grayscale:  # grayscale only
+                open_cv_image = np.array(captured)  # Convert captured img to cv2 format
+                open_cv_image = open_cv_image[:, :, ::-1].copy()  # Convert RGB to BGR
+                grayImg = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)  # Convert the image to gray scale
 
-                # Convert the image to gray scale
-                grayImg = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
-
-                if debugmode and grayScale:
+                if debugmode:
                     cv2.imshow("Grayscale Image", grayImg)
 
-            # Get the text from the image
-            wordsGet = pytesseract.image_to_string(grayImg, langCode)
+                wordsGet = pytesseract.image_to_string(grayImg, langCode)
+            else:  # no enhancement
+                wordsGet = pytesseract.image_to_string(captured, langCode)
 
             if saveImg:
                 createPicDirIfGone()
-                captured.save(os.path.join(img_captured_path, "captured_" + datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".png"))
+                captured.save(saveName)
 
+        wordsGet = wordsGet.strip().replace("\n", " ") if replaceNewLine else wordsGet.strip()
         is_Success = True
     except Exception as e:
-        print("Error: " + str(e))
+        logger.exception("Error: " + str(e))
         wordsGet = str(e)
         if "is not installed or it's not in your PATH" in str(e):
             Mbox("Error: Tesseract Could not be Found", "Invalid path location for tesseract.exe, please change it in the setting!", 2)
@@ -175,12 +165,6 @@ def captureImg(coords, sourceLang, tesseract_Location, saveImg=False, enhance_Wi
         else:
             Mbox("Error", str(e), 2)
     finally:
-        # replace all \n with space
-        if replaceNewLine:
-            wordsGet = wordsGet.strip().replace("\n", " ")
-        else:
-            wordsGet = wordsGet.strip()
-
         return is_Success, wordsGet
 
 
@@ -188,12 +172,13 @@ def captureAll():
     """Capture all screens and save the result"""
     # Capture all screens
     try:
-        captured = pyautogui.screenshot()
+        captured = pyautogui.screenshot()  # type: ignore
+        saveName = os.path.join(dir_captured, "Full Captured Monitor(s) View.png")
         createPicDirIfGone()
-        captured.save(os.path.join(img_captured_path, "Monitor(s) Captured View" + ".png"))
-        startfile(dir_path + r"\..\img_captured\Monitor(s) Captured View.png")
+        captured.save(saveName)
+        startFile(saveName)
     except Exception as e:
-        print("Error: " + str(e))
+        logger.exception("Error: " + str(e))
         if "Invalid argument" in str(e):
             Mbox("Error image is still opened", "Please close the previous image first!", 2)
         else:
